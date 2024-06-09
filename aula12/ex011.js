d486d5c4a532606e27a88fb269a0ab0413af7452
Jardel Pereira <jardel_pereira1@outlook.com>
2024-06-09 00:02:17 -0300

    exercicios 11, 12 e 13

---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+// condição aninhada
+
+var idade = 65
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade < 18 || idade >= 65) {
+    console.log('Voto facultativo')
+} else {
+    console.log("Voto obrgatório")
+}
