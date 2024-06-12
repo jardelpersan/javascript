60d668d063b50e471ed03ff0d24a960f7e9d0b54
Jardel Pereira <jardel_pereira1@outlook.com>
2024-06-12 00:11:01 -0300

    testes

--- exercicios/modelo01/script.js
+++ exercicios/modelo01/script.js
@@ -1,1 +1,17 @@
-alert('ola')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var minuts = data.getMinutes()
+    msg.innerHTML = `Agora são ${hora}:${minuts}h.`
+
+    if (hora >= 6 && hora < 12) {
+        img.src = "manha.jpg"
+        
+        } else if (hora >=12 && hora < 18) {
+            img.src = "tarde.jpg"
+        } else {
+            img.src = "noite.jpg"
+        }
+}
