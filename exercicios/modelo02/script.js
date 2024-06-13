26bbd345b770bc417bc12779977990fd8fed5132
Jardel Pereira <jardel_pereira1@outlook.com>
2024-06-12 23:37:22 -0300

    modelo 2

--- exercicios/modelo02/script.js
+++ exercicios/modelo02/script.js
@@ -1,1 +1,22 @@
-alert('ola')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value == 0 || fano.value > ano) {
+        window.alert("Verifique os dados e tente novamente")
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        //res.innerHTML = `Idade calculada: ${idade}`
+        var genero = ''
+        var img = document.createElement('img')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+        } else if (fesx[1].checked) {
+            genero = 'Mulher'
+        }
+        res.style.textAlign='center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+    }
+}
