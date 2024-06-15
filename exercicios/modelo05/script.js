5fd2cc180e96bf82dc6843691a4e186f9c0d521b
Jardel Pereira <jardel_pereira1@outlook.com>
2024-06-14 22:50:07 -0300

    inicio

---
+++ exercicios/modelo05/script.js
@@ -0,0 +1,26 @@
+let num = document.getElementById('fnum')
+let lista = document.getElementById('flista')
+let res = document.getElementById('res')
+let valores = []
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+function isLista(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if (isNumero(num.value) && !isLista(num.value, valores)) {
+} else {
+    alert('valor inválido ou já encontrado na lista.')
+}
+}
